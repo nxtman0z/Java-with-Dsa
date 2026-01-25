88b0d68902d295bc9d5580c078dd23d4cc9c848f
Manoj <manojkumarpanigrahi54@gmail.com>
2026-01-25 20:35:18 +0530

    spiral problem

--- 8-2D Array/spiral.java
+++ 8-2D Array/spiral.java
@@ -39,7 +39,7 @@ public class spiral {
     public static void main(String[] args) {
         int matrix[][] = {{1,2,3,4},
                             {5,6,7,8},
-                            {9,10,11,12},
+                            {9,10,18,12},
                             {13,14,15,16}};
         spiralMatrix(matrix);
     }
